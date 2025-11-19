bbc8cb9bb5a9fb08e0b9c5ac34a0aed25514b6c4
Ahmad Muzakkir Bin Mohamad <muzakkir@Ahmads-MacBook-Air.local>
2025-11-19 22:41:00 +0800

    test cross development

--- my-react-app/src/components/LetterPage.jsx
+++ my-react-app/src/components/LetterPage.jsx
@@ -5,7 +5,8 @@ function LetterPage ({ letter, data, onBack }) {
     return (
         <div className={styles.container}>
             <button onClick={onBack} className={styles.backButton}>Back</button>
-            <h1>{letter} for {data.word}</h1>
+            <h1>{letter} for the {data.word}</h1>
+            <p>test</p>
             <img src={data.image} alt={data.word} className={styles.image} />
         </div>
     );
