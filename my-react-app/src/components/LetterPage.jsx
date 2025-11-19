86b0b7746aa16a0a35aef38a38061afe6da9362a
arekay17 <arekay17@gmail.com>
2025-11-19 07:35:58 +0000

    refactor css for letterPage

--- my-react-app/src/components/LetterPage.jsx
+++ my-react-app/src/components/LetterPage.jsx
@@ -1,31 +1,14 @@
 import React from 'react';
+import styles from './LetterPage.module.css';
 
 function LetterPage ({ letter, data, onBack }) {
     return (
-        <div style={styles.container}>
-            <button onClick={onBack} style={styles.backButton}>Back</button>
+        <div className={styles.container}>
+            <button onClick={onBack} className={styles.backButton}>Back</button>
             <h1>{letter} for {data.word}</h1>
-            <img src={data.image} alt={data.word} style={styles.image} />
+            <img src={data.image} alt={data.word} className={styles.image} />
         </div>
     );
 }
-
-const styles = {
-    container: {
-        textAlign: 'center',
-        padding: '20px',
-    },
-    backButton: {
-        padding: '10px 20px',
-        marginBottom: '20px',
-        cursorr: 'pointer',
-    },
-    image: {
-        width: '250px',
-        borderradius: '15px',
-        marginTop: '20px',
-    },
-};
-
 
 export default LetterPage;
