4dd447b6a6553fed1624c6b078669ce63e6cbede
arekay17 <arekay17@gmail.com>
2025-11-23 08:58:05 +0000

    adjust css for letterpage

--- my-react-app/src/components/LetterPage.jsx
+++ my-react-app/src/components/LetterPage.jsx
@@ -1,14 +1,26 @@
 import React from 'react';
 import styles from './LetterPage.module.css';
 
-function LetterPage ({ letter, data, onBack }) {
-    return (
-        <div className={styles.container}>
-            <button onClick={onBack} className={styles.backButton}>Back</button>
-            <h1>{letter} for {data.word}</h1>
-            <img src={data.image} alt={data.word} className={styles.image} />
-        </div>
-    );
+function LetterPage({ letter, data, onBack }) {
+  return (
+    <div className={styles.container}>
+      <button onClick={onBack} className={styles.backButton}>
+        ⬅ Kembali
+      </button>
+
+      <h1 className={styles.heading}>
+        {letter} for <span className={styles.word}>{data.word}</span>
+      </h1>
+
+      <div className={styles.imageWrapper}>
+        <img
+          src={data.image}
+          alt={data.word}
+          className={styles.image}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default LetterPage;
