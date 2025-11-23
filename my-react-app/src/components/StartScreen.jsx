0fa86fff64c99c3be8a3b8f64fbd402e1f387a84
arekay17 <arekay17@gmail.com>
2025-11-23 08:47:29 +0000

    change startcreen css

--- my-react-app/src/components/StartScreen.jsx
+++ my-react-app/src/components/StartScreen.jsx
@@ -6,7 +6,7 @@ function StartScreen ({onSelectMode}){
         <div className={styles.startContainer}>
             <h1 className={styles.heading}>Pilih Mode Belajar</h1>
             <div className={styles.button}>
-                <button className={`${styles.modeButton} ${styles.primary}`} onClick={() => onSelectMode('all')} aria-label="Belajar Semua Huruf">Belajar Semua Huruf</button>
+                <button className={styles.modeButton} onClick={() => onSelectMode('all')} aria-label="Belajar Semua Huruf">Belajar Semua Huruf</button>
                 <button className={styles.modeButton} onClick={() => onSelectMode('vowels')} aria-label="Learn Vowels">Huruf Vokal</button>
                 <button className={styles.modeButton} onClick={() => onSelectMode('syllable')} aria-label="Learn Syllables">Suku Kata</button>
                 <button className={styles.modeButton} onClick={() => onSelectMode('simple')} aria-label="Learn Simple Words">Belajar Perkataan Ringkas</button>
